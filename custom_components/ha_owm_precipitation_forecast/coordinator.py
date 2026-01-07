ea7d7eb331c5a73722f9e7ba79667e1dd0aaddaa
Jeff Jensen <jjensen@apache.org>
2026-01-07 12:52:56 -0600

    fix import

--- custom_components/ha_owm_precipitation_forecast/coordinator.py
+++ custom_components/ha_owm_precipitation_forecast/coordinator.py
@@ -1,5 +1,5 @@
-from typing import Any
 from __future__ import annotations
+from typing import Any
 import logging
 from datetime import timedelta
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, UpdateFailed
